19bcfaf76da5d0153431b1164f8df9c67ab84f66
Franck CICCOTTI <franckciccotti@me.com>
2023-07-18 13:37:23 +0200

    update services video, category, section and user

--- src/app/core/services/section.service.ts
+++ src/app/core/services/section.service.ts
@@ -1,9 +1,70 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SectionService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+  url = environment.apiUrl;
+
+  // ADD A SECTION
+  createSection(body: any) : Observable<any>{
+    return this.http.post<any>(
+      this.url+"sections/create-section",
+      body
+    );
+  }
+
+  // GET ALL SECTIONS
+  getAllSection() : Observable<any>{
+    return this.http.get(
+      this.url+"sections"
+    );
+  }
+
+  // GET A SECTION BY ID
+  getSectionById(body: any) : Observable<any>{
+    return this.http.post<any>(
+      this.url+"sections/{id}",
+      body
+    );
+  }
+
+  // UPDATE A SECTION
+  updateSection(body: any) : Observable<any>{
+    return this.http.put<any>(
+      this.url+"sections/update-section",
+      body
+    );
+  }
+
+  // DELETE A SECTION BY ID
+  removeSection(body: any) : Observable<any>{
+    return this.http.delete<any>(
+      this.url+"sections/remove-section/{id}",
+      body
+    );
+  }
+
+  // ADD A VIDEO BY ID
+  addVideoToSectionById(body: any) : Observable<any>{
+    return this.http.post<any>(
+      this.url+"sections/add-video-to-section",
+      body
+    );
+  }
+
+  // DELETE A VIDEO BY ID
+  removeVideoToSection(body: any) : Observable<any>{
+    return this.http.delete<any>(
+      this.url+"sections/remove-video-to-section",
+      body
+    );
+  }
+
 }
--- src/app/core/services/video.service.ts
+++ src/app/core/services/video.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -12,14 +12,67 @@ export class VideoService {
 
   url = environment.apiUrl;
 
-  upload (body: any) : Observable<HttpResponse<any>>{
+  // ADD UNE VIDEO
+  upload(body: any) : Observable<any>{
     return this.http.post<any>(
       this.url+"videos/upload-video",
       body
     )
   }
 
-  getAllVideos () : Observable<any>{
-    return this.http.get(this.url+"videos")
+  // GET TOUTES LES VIDEOS
+  getAllVideos() : Observable<any>{
+    return this.http.get(
+      this.url+"videos"
+      )
   }
+
+  // GET UNE VIDEO BY ID
+  getVideoById(body : any) : Observable<any>{
+    return this.http.post<any>(
+      this.url+"videos/{id}",
+      body
+    );
+  }
+
+  // SUPPRIMER UNE VIDEO
+  removeVideo(body : any) : Observable<any>{
+    return this.http.delete<any>(
+      this.url+"videos/remove",
+      body
+    );
+  }
+  
+  // MODIFIER UNE VIDEO
+  updateVideo(body : any) : Observable<any>{
+    return this.http.put<any>(
+      this.url+"videos/update-video",
+      body
+    );
+  }
+  
+  //GET TOUTES LES VIDEOS PUBLIC
+  getAllPublicVideos(body : any) : Observable<any>{
+    return this.http.post<any>(
+      this.url+"videos/public/{isPrivate}",
+      body
+    );
+  }
+  
+  // ADD CATEGORY TO VIDEO BY Id
+  addCategoryToVideoById(body : any) : Observable<any>{
+    return this.http.post<any>(
+      this.url+"videos/add-category",
+      body
+    );
+  }
+    
+  // GET VIDEOS BY CATEGORY
+  getVideosByCategory(body : any) : Observable<any>{
+    return this.http.post<any>(
+      this.url+"videos/by-category/{idCategory}",
+      body
+    );
+  }
+
 }
